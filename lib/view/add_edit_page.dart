a614c3c526fdc8cf980b61e85ff63e78b0c9d214
mayan <mayanksanecha433@gmail.com>
2024-10-11 08:38:14 +0530

    Initial commit

---
+++ lib/view/add_edit_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../todo_cubit.dart';
+import '../todo_model.dart';
+
+class AddEditPage extends StatelessWidget{
+  bool isupdate;
+  int sno;
+  String title,desc;
+  AddEditPage({this.isupdate=false,this.title="",this.desc="",this.sno=0});
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    if(isupdate){
+      titleController.text=title;
+      descController.text=desc;
+    }
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      appBar: AppBar(
+        title: Text(isupdate?"Update Todo":"Add Todo"),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: titleController,
+            decoration: InputDecoration(
+                hintText: "Enter the title "
+            ),
+          ),
+          SizedBox(height: 15,),
+          TextField(
+            controller: descController,
+            decoration: InputDecoration(
+                hintText: "Enter the description"
+            ),
+          ),
+          SizedBox(height: 25,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(onPressed: (){
+                isupdate?context.read<TodoCubit>().updateTodo(TodoModel(title: titleController.text, desc: descController.text, created: DateTime.now().millisecondsSinceEpoch.toString()), sno):context.read<TodoCubit>().addTodo(TodoModel(title: titleController.text, desc: descController.text, created: DateTime.now().millisecondsSinceEpoch.toString()));
+              }, child: Text(isupdate?"Update":"Add")),
+              ElevatedButton(onPressed: (){
+                Navigator.pop(context);
+              }, child: Text("cancel"))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+}
